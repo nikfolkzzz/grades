a0fc979633de20d5af18997e1d8eb9e4d195cb2a
Andru Nikolaev <aeanst514@yandex.ru>
2019-08-08 14:22:10 +0000

    sort str arr

---
+++ js/sortStr.js
@@ -0,0 +1,12 @@
+let arr = ["HTML", "JavaScript", "CSS"];
+const  sortArr = (arr) => {
+
+   return sortedArr = [...arr].sort()
+
+
+}
+
+newArr = sortArr(arr);
+console.log('new :',newArr)
+console.log('old', arr)
+
