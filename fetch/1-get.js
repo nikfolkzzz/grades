75c3b9a93a7d84f95d8d6e9428ad66933bce326b
Andru Nikolaev <aeanst514@yandex.ru>
2019-09-08 20:05:57 +0400

    add fetch

---
+++ fetch/1-get.js
@@ -0,0 +1,21 @@
+"use strict";
+const http = require("http");
+const url = "http://ietf.org";
+http.get(url, res => {
+  console.log(res.req._header);
+  console.dir(res.headers);
+  if (res.statusCode != 200) {
+    const { statusCode, statusMessage } = res;
+    console.log(`status code: ${statusCode} ${statusMessage}`);
+    return;
+  }
+
+  res.setEncoding("utf8");
+  const buffer = [];
+  res.on("data", chunk => {
+    buffer.push(chunk);
+  });
+  res.on("end", () => {
+    console.log(buffer.join());
+  });
+});
